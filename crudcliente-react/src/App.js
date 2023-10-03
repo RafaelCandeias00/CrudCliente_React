c8cfcfc976916f2d20e1d7cfb74193489ec89ce1
RafaelCandeias00 <rafaelcandeias39@gmail.com>
2023-10-03 13:21:19 -0300

    Iniciando projeto | add bootstrap, axios e reactstrap

---
+++ crudcliente-react/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import {Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Olá Rafael!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
